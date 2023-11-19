90b4c5727d3eb6d1e1c3254b5e423152fb1a54ad
JerryAgbesi <jerryeagbesi@gmail.com>
2023-11-19 10:13:32 +0000

    add user model and validation

---
+++ test/models/user_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test "user with valid email should be valid" do
+    user = User.create(email: "one@gmail.com",password_digest: "person1_pass")
+    assert user.valid?
+  end
+
+  test "user with invalid email should be invalid" do
+    user = User.create(email: "person1ingmail.com",password_digest: "person1_pass")
+    assert_not user.valid?
+  end
+
+  test "user with taken email should be invalid" do
+    existing_user = users(:one)
+    user = User.create(email: existing_user.email ,password_digest: "person1_pass")
+    assert_not user.valid?
+  end
+end
